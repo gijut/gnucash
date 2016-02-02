a776fa8f5a8dc08982730915fd1b72d535621a13
Gijut <gLETTERyYuMEANSjLETTERyOt>
2016-02-02 13:25:11 +0100

    a date without time should mean 1100Z instead of current time

--- src/libqof/qof/gnc-date.cpp
+++ src/libqof/qof/gnc-date.cpp
@@ -760,8 +760,8 @@ gnc_parse_time_date (struct tm *parsed, const char * buff)
     secs = time(NULL); /* gnc_time(NULL) or time(NULL) ?? */
     gnc_localtime_r(&secs, parsed);
     tz = parsed->tm_gmtoff;
-    if (TZhourtxt != NULL) {
-        if (dupe[0] != ':' && minutetxt) {/* "hh:mm date" and "hh:mm:ss date" are parsed. "hh date" is not recognized. "hh:mm" and "hh:mm:ss" imply today. Default value for the timezone is TZenv */
+    if (TZhourtxt || minutetxt) {
+        if (dupe[0] != ':') {/* "hh:mm date" and "hh:mm:ss date" are parsed. "hh date" is not recognized. "hh:mm" and "hh:mm:ss" imply today. Default value for the timezone is TZenv */
             secondtxt = strtok (NULL, " ");
             tmp = strtok (NULL, "\a");/* hack: \a is the audible bell, hope this one is never used in a date format ; tmp = minutetxt + strlen (strtok (minutetxt, " "))+1 might be better */
             TZtxt = strtok (TZhourtxt, " ");
